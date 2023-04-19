b7db666c1d0febf4cc92bdde88832275518e4bb9
rwanve <88335827+rwanve@users.noreply.github.com>
2023-04-19 19:45:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 resource "aws_instance" "Demo" {
   ami           = "ami-05c8ca4485f8b138a"
-  instance_type = "t2.micro"
+  instance_type = "t2.large"
   key_name = "aaa"
   tags = {
     Name = "webserver"
